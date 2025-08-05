0c1e5c517264f60be551e827e114337551502401
Stefan Bischof <stbischof@bipolis.org>
2025-08-05 18:42:39 +0200

    extract constants into an api package
    Signed-off-by: Stefan Bischof <stbischof@bipolis.org>

---
+++ common/src/main/java/org/eclipse/daanse/olap/core/api/Constants.java
@@ -0,0 +1,23 @@
+/*
+* Copyright (c) 2025 Contributors to the Eclipse Foundation.
+*
+* This program and the accompanying materials are made
+* available under the terms of the Eclipse Public License 2.0
+* which is available at https://www.eclipse.org/legal/epl-2.0/
+*
+* SPDX-License-Identifier: EPL-2.0
+*
+* Contributors:
+*   SmartCity Jena - initial
+*/
+package org.eclipse.daanse.olap.core.api;
+
+public class Constants {
+
+    private Constants() {
+    }
+
+    public static final String BASIC_CONTEXT_GROUP_PID = "org.eclipse.daanse.olap.core.BasicContextGroup";
+
+    public static final String BASIC_CONTEXT_GROUP_REF_NAME_CONTEXTS = "context";
+}

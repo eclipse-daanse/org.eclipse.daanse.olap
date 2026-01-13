f6237556759c817bc46387b19312e74769fcd7e7
Stefan Bischof <stbischof@bipolis.org>
2026-01-13 08:11:31 +0100

    refactor AbstractProfilingNestedDoubleCalc rm obsolet code
    Signed-off-by: Stefan Bischof <stbischof@bipolis.org>

--- common/src/main/java/org/eclipse/daanse/olap/calc/base/nested/AbstractProfilingNestedDoubleCalc.java
+++ common/src/main/java/org/eclipse/daanse/olap/calc/base/nested/AbstractProfilingNestedDoubleCalc.java
@@ -14,38 +14,23 @@
 
 package org.eclipse.daanse.olap.calc.base.nested;
 
-import org.eclipse.daanse.olap.api.Evaluator;
 import org.eclipse.daanse.olap.api.calc.Calc;
 import org.eclipse.daanse.olap.api.calc.DoubleCalc;
 import org.eclipse.daanse.olap.api.type.NumericType;
 import org.eclipse.daanse.olap.api.type.Type;
 import org.eclipse.daanse.olap.calc.base.AbstractProfilingNestedCalc;
-import org.eclipse.daanse.olap.fun.FunUtil;
 
 /**
  * Abstract implementation of the
- * {@link org.eclipse.daanse.olap.api.calc.IntegerCalc} interface.
- * 
- * Handles nested child and profiling
+ * {@link org.eclipse.daanse.olap.api.calc.DoubleCalc} interface.
  *
+ * Handles nested child and profiling.
  */
 public abstract class AbstractProfilingNestedDoubleCalc extends AbstractProfilingNestedCalc<Double>
 		implements DoubleCalc {
-	/**
-	 * {@inheritDoc}
-	 *
-	 */
+
 	protected AbstractProfilingNestedDoubleCalc(Type type, Calc<?>... calcs) {
 		super(type, calcs);
 		requiresType(NumericType.class);
-	}
-
-	@Override
-	public Double evaluateInternal(Evaluator evaluator) {
-		final Double d = evaluate(evaluator);
-		if (d == FunUtil.DOUBLE_NULL) {
-			return null;
-		}
-		return Double.valueOf(d);
 	}
 }

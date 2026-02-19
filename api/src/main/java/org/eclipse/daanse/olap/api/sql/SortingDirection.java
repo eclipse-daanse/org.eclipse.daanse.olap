c92887ca537c7954c0bea29a3e79865cc8ae438f
dbulahov <bulahovdenis@gmail.com>
2026-02-19 14:29:44 +0300

    add enumerator SortingDirection to OrderedColumn Signed-off-by: dbulahov <bulahovdenis@gmail.com>

--- api/src/main/java/org/eclipse/daanse/olap/api/sql/SortingDirection.java
+++ api/src/main/java/org/eclipse/daanse/olap/api/sql/SortingDirection.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2025 Contributors to the Eclipse Foundation.
+ * Copyright (c) 2026 Contributors to the Eclipse Foundation.
  *
  * This program and the accompanying materials are made
  * available under the terms of the Eclipse Public License 2.0
@@ -13,14 +13,6 @@
  */
 package org.eclipse.daanse.olap.api.sql;
 
-import java.util.List;
-
-import org.eclipse.daanse.olap.api.SqlStatement;
-
-public interface SqlExpression {
-
-    List<SqlStatement> getSqls();
-
-    SortingDirection getSortingDirection();
-
+public enum SortingDirection {
+    ASC, DESC, NONE;
 }

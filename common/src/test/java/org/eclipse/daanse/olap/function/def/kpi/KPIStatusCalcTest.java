a8ee8de904a05dbcc18d82747215ac6f62fd6c69
dbulahov <bulahovdenis@gmail.com>
2026-03-18 17:39:09 +0300

    junit tests for kpi Signed-off-by: dbulahov <bulahovdenis@gmail.com>

---
+++ common/src/test/java/org/eclipse/daanse/olap/function/def/kpi/KPIStatusCalcTest.java
@@ -0,0 +1,105 @@
+/*
+ * Copyright (c) 2026 Contributors to the Eclipse Foundation.
+ *
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *   SmartCity Jena - initial
+ *   Stefan Bischof (bipolis.org) - initial
+ */
+package org.eclipse.daanse.olap.function.def.kpi;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.isNull;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.mockStatic;
+import static org.mockito.Mockito.when;
+
+import java.util.List;
+
+import org.eclipse.daanse.olap.api.Context;
+import org.eclipse.daanse.olap.api.calc.StringCalc;
+import org.eclipse.daanse.olap.api.catalog.CatalogReader;
+import org.eclipse.daanse.olap.api.connection.Connection;
+import org.eclipse.daanse.olap.api.element.Catalog;
+import org.eclipse.daanse.olap.api.element.Cube;
+import org.eclipse.daanse.olap.api.element.KPI;
+import org.eclipse.daanse.olap.api.element.Member;
+import org.eclipse.daanse.olap.api.evaluator.Evaluator;
+import org.eclipse.daanse.olap.api.type.MemberType;
+import org.eclipse.daanse.olap.common.ConfigConstants;
+import org.eclipse.daanse.olap.fun.FunUtil;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.MockedStatic;
+
+class KPIStatusCalcTest {
+
+    private KPIStatusCalc kpiStatusCalc;
+    private StringCalc stringCalc;
+    private Evaluator evaluator;
+    private Cube cube;
+    private KPI kpi;
+    private Member expectedMember;
+    private Catalog catalog;
+    private Connection connection;
+    private Context context;
+    private CatalogReader catalogReader;
+
+
+    @BeforeEach
+    void setUp() {
+        stringCalc = mock(StringCalc.class);
+        evaluator = mock(Evaluator.class);
+        cube = mock(Cube.class);
+        kpi = mock(KPI.class);
+        catalog = mock(Catalog.class);
+        connection = mock(Connection.class);
+        context = mock(Context.class);
+        expectedMember = mock(Member.class);
+        catalogReader = mock(CatalogReader.class);
+        kpiStatusCalc = new KPIStatusCalc(MemberType.Unknown, stringCalc);
+        when(evaluator.getCube()).thenReturn(cube);
+        when(cube.getCatalog()).thenReturn(catalog);
+        when(catalog.getInternalConnection()).thenReturn(connection);
+        when(connection.getContext()).thenReturn(context);
+        when(context.getConfigValue(ConfigConstants.IGNORE_INVALID_MEMBERS, true, Boolean.class)).thenReturn(true);
+        when(context.getConfigValue(ConfigConstants.IGNORE_INVALID_MEMBERS_DURING_QUERY, false, Boolean.class)).thenReturn(true);
+        when(evaluator.getCatalogReader()).thenReturn(catalogReader);
+        when(catalogReader.getCalculatedMember(any())).thenReturn(expectedMember);
+    }
+
+    @Test
+    @DisplayName("Should return member when KPI is found")
+    void shouldReturnMemberWhenKpiFound() {
+        when(stringCalc.evaluate(evaluator)).thenReturn("RevenueKpi");
+        when(kpi.getName()).thenReturn("RevenueKpi");
+        when(kpi.getStatus()).thenReturn("[Measures].[Status]");
+        when(cube.getKPIs()).thenReturn((List) List.of(kpi));
+        Member result = kpiStatusCalc.evaluate(evaluator);
+    }
+
+    @Test
+    @DisplayName("Should throw exception when KPI is not found")
+    void shouldThrowExceptionWhenKpiNotFound() {
+        when(stringCalc.evaluate(evaluator)).thenReturn("UnknownKpi");
+        when(cube.getKPIs()).thenReturn((List) List.of());
+
+        assertThatThrownBy(() -> kpiStatusCalc.evaluate(evaluator))
+                .isInstanceOf(RuntimeException.class)
+                .hasMessageContaining("UnknownKpi");
+    }
+
+    @Test
+    @DisplayName("Should have MemberType as type")
+    void shouldHaveMemberType() {
+        assertThat(kpiStatusCalc.getType()).isEqualTo(MemberType.Unknown);
+    }
+}
---
+++ common/src/test/java/org/eclipse/daanse/olap/function/def/kpi/KPIValueFunDefTest.java
@@ -0,0 +1,76 @@
+/*
+ * Copyright (c) 2026 Contributors to the Eclipse Foundation.
+ *
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *   SmartCity Jena - initial
+ *   Stefan Bischof (bipolis.org) - initial
+ */
+package org.eclipse.daanse.olap.function.def.kpi;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import org.eclipse.daanse.olap.api.DataType;
+import org.eclipse.daanse.olap.api.calc.Calc;
+import org.eclipse.daanse.olap.api.calc.StringCalc;
+import org.eclipse.daanse.olap.api.calc.compiler.ExpressionCompiler;
+import org.eclipse.daanse.olap.api.query.component.Expression;
+import org.eclipse.daanse.olap.api.query.component.ResolvedFunCall;
+import org.eclipse.daanse.olap.api.type.MemberType;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class KPIValueFunDefTest {
+
+    private KPIValueFunDef funDef;
+
+    @BeforeEach
+    void setUp() {
+        funDef = new KPIValueFunDef();
+    }
+
+    @Test
+    @DisplayName("Should have function name KPIValue")
+    void shouldHaveCorrectFunctionName() {
+        assertThat(funDef.getFunctionMetaData().operationAtom().name()).isEqualTo("KPIValue");
+    }
+
+    @Test
+    @DisplayName("Should return MEMBER data type")
+    void shouldReturnMemberDataType() {
+        assertThat(funDef.getFunctionMetaData().returnCategory()).isEqualTo(DataType.MEMBER);
+    }
+
+    @Test
+    @DisplayName("Should have one STRING parameter")
+    void shouldHaveOneStringParameter() {
+        DataType[] paramTypes = funDef.getFunctionMetaData().parameterDataTypes();
+        assertThat(paramTypes).hasSize(1);
+        assertThat(paramTypes[0]).isEqualTo(DataType.STRING);
+    }
+
+    @Test
+    @DisplayName("Should compile call to KPIValueCalc")
+    void shouldCompileToKPIValueCalc() {
+        ResolvedFunCall call = mock(ResolvedFunCall.class);
+        ExpressionCompiler compiler = mock(ExpressionCompiler.class);
+        Expression arg = mock(Expression.class);
+        StringCalc stringCalc = mock(StringCalc.class);
+
+        when(call.getArg(0)).thenReturn(arg);
+        when(call.getType()).thenReturn(MemberType.Unknown);
+        when(compiler.compileString(arg)).thenReturn(stringCalc);
+
+        Calc<?> result = funDef.compileCall(call, compiler);
+
+        assertThat(result).isInstanceOf(KPIValueCalc.class);
+    }
+}

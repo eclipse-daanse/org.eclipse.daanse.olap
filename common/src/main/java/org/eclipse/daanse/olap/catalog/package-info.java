c4ea5f993f5900676a0db33a7d353dbf7fe4f7e3
Stefan Bischof <stbischof@bipolis.org>
2026-09-15 21:22:38 +0200

    feat(catalog,writeback,calc,aggregator): small pieces from rolap
    ContentIdentity, RolapCatalogContentKey as CatalogContentKey -> olap.catalog;
    PendingFact, Updateable -> olap.writeback; TupleConstraintStruct -> olap.calc.base.util;
    CustAggregatorFactory as CustomAggregatorFactoryImpl -> olap.aggregator
    
    Signed-off-by: Stefan Bischof <stbischof@bipolis.org>

---
+++ common/src/main/java/org/eclipse/daanse/olap/catalog/package-info.java
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) 2026 Contributors to the Eclipse Foundation.
+ *
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *   SmartCity Jena - initial
+ *   Stefan Bischof (bipolis.org) - initial
+ */
+
+/**
+ * Content-based identity of a catalog: the fingerprint a pool keys its entries by.
+ */
+@org.osgi.annotation.bundle.Export
+@org.osgi.annotation.versioning.Version("0.0.1")
+package org.eclipse.daanse.olap.catalog;
